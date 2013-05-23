f6249e23b1dd5fb5192023376d05405bf624c1b4
Paulo Choi <mail@paulochoi.com>
2013-05-23 19:27:28 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RedditOnRails::Application.config.secret_token = '4e3436fb7ac439532b4f98c9b9c6d018385844b8d81b00a115874209f5c3ed301cbe35a8e7229e9eebd42aea140c0f07bc4a12c55c9f2115f6d4f3655dc93107'

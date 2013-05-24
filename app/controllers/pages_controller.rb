0a9de54efad2f581c4c017f0650c588db3b92235
Paulo Choi <mail@paulochoi.com>
2013-05-23 23:27:14 -0400

    add pagination to main page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,9 @@
 class PagesController < ApplicationController
 
 	def index
-		@links = Link.all
+		params[:per_page] ||= 25
+		params[:page]     ||= 1
+		@links = Link.order('created_at DESC').page(params[:page].to_i).per_page(params[:per_page].to_i)
 	end
 
 end

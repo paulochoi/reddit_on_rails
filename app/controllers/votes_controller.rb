9778e20e7d2d8e9f4cd7e781aa82ba2e7537bef8
Paulo Choi <mail@paulochoi.com>
2013-05-24 14:11:28 -0400

    add protection for upvoting if user not logged on

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,14 +1,15 @@
 class VotesController < ApplicationController
 	def create
-		@vote = Vote.where(link_id: params[:vote][:link_id], user_id: current_user.id).first
+		if current_user
+			@vote = Vote.where(link_id: params[:vote][:link_id], user_id: current_user.id).first
 
-		if @vote
-		  @vote.up = params[:vote][:up]
-		  @vote.save
-		else
-		  @vote = current_user.votes.create(params[:vote])
+			if @vote
+			  @vote.up = params[:vote][:up]
+			  @vote.save
+			else
+			  @vote = current_user.votes.create(params[:vote])
+			end
 		end
-
 		redirect_to :back
 	end
 end

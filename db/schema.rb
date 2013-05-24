c55eb786ae40a4c10bb29d8e4b8a240cde9ef32b
Paulo Choi <mail@paulochoi.com>
2013-05-23 23:53:50 -0400

    add comment model, associate with user and link model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130524004209) do
+ActiveRecord::Schema.define(:version => 20130524034537) do
+
+  create_table "comments", :force => true do |t|
+    t.text     "message"
+    t.integer  "user_id"
+    t.integer  "link_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "links", :force => true do |t|
     t.string   "url"

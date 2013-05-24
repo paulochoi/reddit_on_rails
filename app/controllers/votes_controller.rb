3c5bca02639f68fd3cae4ac5b750b556b057819a
Paulo Choi <mail@paulochoi.com>
2013-05-24 13:23:58 -0400

    add bootstrap layout to main page

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,6 +1,6 @@
 class VotesController < ApplicationController
 	def create
-		@vote = Vote.where(:link_id => params[:vote][:link_id], :user_id => current_user.id).first
+		@vote = Vote.where(link_id: params[:vote][:link_id], user_id: current_user.id).first
 
 		if @vote
 		  @vote.up = params[:vote][:up]
@@ -8,7 +8,7 @@ class VotesController < ApplicationController
 		else
 		  @vote = current_user.votes.create(params[:vote])
 		end
-		
+
 		redirect_to :back
 	end
 end
